53c079251de4f122ab1ddaac6e6c95487ebb018a
Allen, Chuang <77716418+2Allen@users.noreply.github.com>
2024-07-01 14:12:59 +0800

    edit train_model_de.py
    更新註解

--- train/train_model_ds.py
+++ train/train_model_ds.py
@@ -1,58 +1,48 @@
 # %%
 import os
+from datasets import load_dataset
+from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments, Trainer
+
+# 40系顯卡必須使用
 os.environ["NCCL_P2P_DISABLE"] = "1"
 os.environ["NCCL_IB_DISABLE"] = "1"
+
 os.environ['TOKENIZERS_PARALLELISM'] = "True"
 
-# %%
-from datasets import load_dataset
-
+# 載入資料集
 ds = load_dataset("HuggingFaceH4/no_robots", trust_remote_code=True)
 ds
 
-# %%
-from transformers import AutoModel, AutoModelForCausalLM, AutoTokenizer, TrainingArguments, Trainer
 
-
+# 載入模型
 model_id = "openai-community/gpt2-large"
-
-
-
-# %%
-tokenizer = AutoTokenizer.from_pretrained(model_id, use_deepspeed=True)
 model = AutoModelForCausalLM.from_pretrained(
     model_id,
     # torch_dtype=torch.bfloat16,
     trust_remote_code=True,
 )
+tokenizer = AutoTokenizer.from_pretrained(model_id, use_deepspeed=True)
 
 
-# %%
-# 确保填充标记设置正确
+# 語言模型標記
 if tokenizer.pad_token is None:
     tokenizer.pad_token = tokenizer.eos_token
     
+# 資料集處理
 def preprocess(examples):
-    # 从 messages 中提取文本内容进行拼接
     def extract_text(messages):
         return " ".join([msg["content"] for msg in messages])
 
     inputs = ["summarize: " + prompt + " " + extract_text(messages) for prompt, messages in zip(examples["prompt"], examples["messages"])]
-    # 对输入和标签进行相同的填充和截断
     model_inputs = tokenizer(inputs, max_length=256, padding='max_length', truncation=True)
     labels = tokenizer(text_target=examples["category"], max_length=256, padding='max_length', truncation=True)
-
-    # 设置 input_ids 和 labels，确保它们长度一致
+    
     model_inputs["labels"] = labels["input_ids"]
-
     return model_inputs
-
-# 处理数据集
 tokenized_dataset = ds.map(preprocess, batched=True, remove_columns=["prompt", "prompt_id", "messages", "category"])
 
 
-
-
+# 設定train參數
 training_args = TrainingArguments(
     output_dir="./results",
     evaluation_strategy="epoch",
@@ -65,7 +55,7 @@ training_args = TrainingArguments(
 )
 
 
-# %%
+# 訓練模型
 trainer = Trainer(
     model=model,
     args=training_args,
